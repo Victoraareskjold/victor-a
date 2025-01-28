70ded9e8f130e65aae191f1860da80ccc5285a0b
Victor Aareskjold <victor.aaareskjold@gmail.com>
2025-01-28 11:31:34 +0100

    updated about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,19 +3,30 @@
 export default function About() {
   return (
     <main className="gap-6">
-      <img src="/profileimg.jpeg" alt="" className="w-48 ml-2 rounded-full" />
+      <img
+        src="/profileimg.jpeg"
+        alt="Profile Picture"
+        className="w-48 ml-2 rounded-full"
+      />
       <p className="dark:text-[var(--color-lightWhite)]">
-        I have always had an interest in technology, and in the past 3 years I
-        have been experimenting with projects, and helped friends and family
-        with tech-solutions.
+        I have always had a strong interest in technology, which has driven me
+        to explore and experiment with various projects over the past three
+        years. During this time, I have honed my problem-solving skills by
+        helping friends and family with diverse tech solutions, from
+        troubleshooting hardware to creating small, tailored applications.
         <br />
         <br />
-        Besides working on these projects, I am currently enrolled in a
-        pre-course for data-engineers. This course includes subjects like math-
-        and physics for enginers improving my understanding of the engineering
-        field.
+        Alongside my personal projects, I am currently enrolled in a pre-course
+        for data engineers. This course has been a valuable opportunity to
+        strengthen my foundation in key subjects such as mathematics and physics
+        for engineers. These studies are not only deepening my understanding of
+        the engineering field but are also equipping me with analytical tools
+        that I can bring into real-world challenges.
         <br />
-        <br />
+        <br />I am passionate about continuous learning and enjoy tackling
+        challenges that push me to grow both technically and personally. Whether
+        it’s exploring new technologies, collaborating with others, or solving
+        complex problems, I am always eager to take on the next opportunity.
       </p>
     </main>
   );
