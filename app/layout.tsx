7dd235e1ae448469b3a4d55405aeda3d3699f845
Victor Aareskjold <victor.aareskjold@icloud.com>
2025-10-18 18:00:42 +0200

    fix: beforeInteractive load

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,8 +45,8 @@ export default function RootLayout({
 
           <main className="antialiased">
             <Script
+              strategy="beforeInteractive"
               src="https://www.consentify.app/api/consent?token=d56cb0a3-ec5d-446b-8fa0-36fd6d8237ab"
-              strategy="lazyOnload" // eller "beforeInteractive"
             ></Script>
 
             {children}
